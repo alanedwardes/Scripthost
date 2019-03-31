cb619ee8b4c6e08d1334c8fc37ec643cf56bc711
Alan Edwardes <alan@alanedwardes.com>
2019-03-31 12:34:25 +0100

    Adds other build targets.

--- publish.ps1
+++ publish.ps1
@@ -2,4 +2,6 @@ Remove-Item publish -Recurse
 
 function Publish-Workshop { dotnet publish src/Scripthost/Scripthost.csproj -r $args -c Release -o $PSScriptRoot/publish/$args }
 
+Publish-Workshop linux-x64
+Publish-Workshop osx-x64
 Publish-Workshop win-x64
